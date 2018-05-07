7450196d7a06f9840f395b20eaefeeec2f98cf40
Gregory Montoir <cyx@users.sourceforge.net>
2018-05-07 11:08:30 +0800

    paf_decoder: dump all videos in no video index is pass as argument

--- tools/pafdec/main.cpp
+++ tools/pafdec/main.cpp
@@ -1,4 +1,5 @@
 
+#include "file.h"
 #include "paf_decoder.h"
 
 int main(int argc, char *argv[]) {
@@ -8,6 +9,21 @@ int main(int argc, char *argv[]) {
 		if (d.Open(argv[1], num)) {
 			d.Decode();
 			d.Close();
+		} else if (num == -1) {
+			File f;
+			if (f.open(argv[1], "rb")) {
+				const int offset = f.readUint32LE();
+				if ((offset & 3) == 0 && (offset / 4) <= PafDecoder::kMaxVideosCount) {
+					for (int i = 0; i < offset / 4; ++i) {
+						fprintf(stdout, "Decoding PAF %d\n", i);
+						PafDecoder d;
+						if (d.Open(argv[1], i)) {
+							d.Decode();
+							d.Close();
+						}
+					}
+				}
+			}
 		}
 	} else {
 		fprintf(stdout, "%s hod.paf [video num]\n", argv[0]);
