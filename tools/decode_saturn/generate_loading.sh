fe8879b469e8a1d05cccbd1915ce26d0076b872c
Gregory Montoir <cyx@users.sourceforge.net>
2018-12-29 21:57:10 +0800

    decode_saturn: generate animated .gif for loading animation

--- tools/decode_saturn/generate_loading.sh
+++ tools/decode_saturn/generate_loading.sh
@@ -3,4 +3,6 @@ mkdir out/
 for f in saturn_loading_*png; do
 	../../libxbr-standalone/test_app $f out/$f xbr2x
 done
-ffmpeg -framerate 10 -pattern_type glob -i 'out/*.png' -c:v libx264 hod_saturn_loading.mp4
+fn=HoD_Saturn_Loading
+ffmpeg -framerate 10 -pattern_type glob -i 'out/*.png' -c:v libx264 $fn.mp4
+convert -delay 10 -loop 0 -layers Optimize 'out/*png $fn.gif
