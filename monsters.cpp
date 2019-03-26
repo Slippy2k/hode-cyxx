160e34717799e1b30cac42383433783eabeeccbe
Gregory Montoir <cyx@users.sourceforge.net>
2019-03-26 13:10:21 +0800

    hode: add remaining arith opcodes

--- monsters.cpp
+++ monsters.cpp
@@ -766,6 +766,93 @@ int Game::runTask_default(Task *t) {
 				arithOp(p[0] - 57, &_mstVars[p[1]], t->localVars[p[2]]);
 			}
 			break;
+		case 67:
+		case 68:
+		case 69:
+		case 70:
+		case 71:
+		case 72:
+		case 73:
+		case 74:
+		case 75:
+		case 76: { // 36
+				MstUnkData *m = 0;
+				if (t->mstObject) {
+					m = t->mstObject->unk8;
+				} else {
+					m = t->dataPtr;
+				}
+				if (m) {
+					assert(p[2] < kMaxLocals);
+					arithOp(p[0] - 67, &m->localVars[p[1]], t->localVars[p[2]]);
+				}
+			}
+			break;
+		case 77:
+		case 78:
+		case 79:
+		case 80:
+		case 81:
+		case 82:
+		case 83:
+		case 84:
+		case 85:
+		case 86: { // 37
+				MstUnkData *m = 0;
+				if (t->mstObject) {
+					m = t->mstObject->unk8;
+				} else {
+					m = t->dataPtr;
+				}
+				if (m) {
+					assert(p[1] < kMaxLocals);
+					arithOp(p[0] - 77, &t->localVars[p[1]], m->localVars[p[2]]);
+				}
+			}
+			break;
+		case 87:
+		case 88:
+		case 89:
+		case 90:
+		case 91:
+		case 92:
+		case 93:
+		case 94:
+		case 95:
+		case 96: { // 38
+				MstUnkData *m = 0;
+				if (t->mstObject) {
+					m = t->mstObject->unk8;
+				} else {
+					m = t->dataPtr;
+				}
+				if (m) {
+					assert(p[1] < kMaxVars);
+					arithOp(p[0] - 87, &_mstVars[p[1]], m->localVars[p[2]]);
+				}
+			}
+			break;
+		case 97:
+		case 98:
+		case 99:
+		case 100:
+		case 101:
+		case 102:
+		case 103:
+		case 104:
+		case 105:
+		case 106: { // 39
+				MstUnkData *m = 0;
+				if (t->mstObject) {
+					m = t->mstObject->unk8;
+				} else {
+					m = t->dataPtr;
+				}
+				if (m) {
+					arithOp(p[0] - 97, &m->localVars[p[1]], m->localVars[p[2]]);
+				}
+			}
+			break;
 		case 107:
 		case 108:
 		case 109:
