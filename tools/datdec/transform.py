1145b3fe7fbf5b6c07cd24bb5dd99227f66a057c
Gregory Montoir <cyx@users.sourceforge.net>
2019-05-22 20:15:23 +0800

    datdec: duplicate the last pixels row for transform data

--- tools/datdec/transform.py
+++ tools/datdec/transform.py
@@ -11,7 +11,7 @@ H = 192
 def transform(input, tdata, output, start = 0):
 	for y in range(0, H):
 		for x in range(0, W):
-			offset = x + ord(tdata[start % len(tdata)])
+			offset = x + ord(tdata[start])
 			start += 1
 			output.putpixel((x, y), input.getpixel((offset % W, y)))
 
@@ -21,9 +21,11 @@ output = Image.new('RGB', (W, H))
 
 assert input.size[0] == W and input.size[1] == H
 
+# duplicate the last pixels row
+tdata += tdata[-256:]
+
 step = 0
-while step * STEP < 256:
+while step * STEP < W:
 	transform(input, tdata, output, step * STEP)
 	output.save('transform_%02d.png' % step)
-	# output.show()
 	step += 1
