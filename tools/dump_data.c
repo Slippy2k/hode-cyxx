defedd8a2c024ed5537f3a0e200f58f7b0589c50
Gregory Montoir <cyx@users.sourceforge.net>
2019-07-22 08:13:39 +0800

    tools/dump_data: detect executable/game version
    This also removes 'dump_staticres' as 'dump_data' already
    handle data dump from the game executable.

--- tools/dump_data.c
+++ tools/dump_data.c
@@ -3,6 +3,21 @@
 #include <stdint.h>
 #include <string.h>
 #include <assert.h>
+#include <zlib.h>
+
+enum {
+	DEMO_FR_12,
+	FULL_FR_10,
+};
+
+struct {
+	uint32_t crc;
+	int version;
+} _versions[] = {
+	{ 0x5429784f, DEMO_FR_12 },
+	{ 0x3110fd89, FULL_FR_10 },
+	{ 0, -1 }
+};
 
 enum DataDumpType {
 	UNSIGNED_8BITS  = 0,
@@ -77,7 +92,7 @@ static void dumpFontCharMap(FILE *fp, const char *name, uint32_t offset, int cou
 	fprintf(stdout, "\n};\n");
 }
 
-static void dumpInt(FILE *fp, const char *name, uint32_t offset, int size, const char *fmt, enum DataDumpType type, uint32_t addr) {
+static void dumpBinary(FILE *fp, const char *name, uint32_t offset, int size, const char *fmt, enum DataDumpType type, uint32_t addr) {
 	int i;
 
 	printf("%s[%d] = {", name, size);
@@ -133,7 +148,7 @@ static void dumpInt(FILE *fp, const char *name, uint32_t offset, int size, const
 	}
 }
 
-static void fillInt(const char *name, int size, const char *fmt, int num) {
+static void dumpZeroes(const char *name, int size, const char *fmt, int num) {
 	int i;
 
 	printf("%s[%d] = {", name, size);
@@ -147,62 +162,95 @@ static void fillInt(const char *name, int size, const char *fmt, int num) {
 	printf("\n};\n");
 }
 
+static int detectVersion(FILE *fp) {
+	int i, count;
+	uint32_t crc;
+	uint8_t buf[1024];
+
+	count = fread(buf, 1, sizeof(buf), fp);
+	if (count <= 0) {
+		fprintf(stderr, "Failed to read %ld bytes, ret %d\n", sizeof(buf), count);
+		return -1;
+	} else if (memcmp(buf, "MZ", 2) != 0) {
+		fprintf(stderr, "Invalid signature '%02x%02x'\n", buf[0], buf[1]);
+		return -1;
+	}
+	crc = crc32(0, Z_NULL, 0);
+	crc = crc32(crc, buf, count);
+	while (1) {
+		count = fread(buf, 1, sizeof(buf), fp);
+		if (count == 0) {
+			break;
+		} else if (count < 0) {
+			fprintf(stderr, "Failed to read %ld bytes, ret %d\n", sizeof(buf), count);
+			return -1;
+		}
+		crc = crc32(crc, buf, count);
+	}
+	for (i = 0; _versions[i].crc != 0; ++i) {
+		if (crc == _versions[i].crc) {
+			return _versions[i].version;
+		}
+	}
+	fprintf(stderr, "Unknown version CRC32 0x%08x\n", crc);
+	return -1;
+}
+
 int main(int argc, char* argv[]) {
-	FILE *fp;
+	if (argc == 2) {
+		FILE *fp = fopen(argv[1], "rb");
+		if (fp) {
 
-	const char *fn = argc > 1 ? argv[1] : "HODWin32_DEMO.exe";
+			const int version = detectVersion(fp);
 
-	fp = fopen(fn, "rb");
+			if (version == DEMO_FR_12) {
 
-	if (fp) {
+//				dumpBinary(fp, "Game::_transformBufferData1", 0x3EA78, 4536, "0x%02X", UNSIGNED_8BITS); // _benchmarkData1
+//				dumpBinary(fp, "Game::_transformBufferData2", 0x3D960, 2152, "0x%02X", UNSIGNED_8BITS); // _benchmarkData2
+//				dumpBinary(fp, "_byte_43E8F8", 0x3E8F8, 68, "0x%02X", UNSIGNED_8BITS);
+//				dumpBinary(fp, "_byte_43E940", 0x3E940, 68, "0x%02X", UNSIGNED_8BITS);
+//				dumpBinary(fp, "_level1OpHelper1KeyMaskTable", 0x3E888, 112, "%d", UNSIGNED_8BITS);
+//				dumpBinary(fp, "_andyMoveTable35", 0x40358, 48, "%3d", SIGNED_8BITS);
+//				dumpBinary(fp, "_actionDirectionKeyMaskTable", 0x3E3D4, 352, "0x%02X", UNSIGNED_8BITS);
+//				dumpBinary(fp, "_dbVolumeTable", 0x40010, 129, "0x%02X", UNSIGNED_8BITS);
 
-	// demo
+				dumpFontCharMap(fp, "_fontCharactersTable", 0x3FE88, 39);
+			}
 
-//		dumpInt(fp, "Game::_transformBufferData1", 0x3EA78, 4536, "0x%02X", UNSIGNED_8BITS); // _benchmarkData1
-//		dumpInt(fp, "Game::_transformBufferData2", 0x3D960, 2152, "0x%02X", UNSIGNED_8BITS); // _benchmarkData2
-//		dumpInt(fp, "_byte_43E8F8", 0x3E8F8, 68, "0x%02X", UNSIGNED_8BITS);
-//		dumpInt(fp, "_byte_43E940", 0x3E940, 68, "0x%02X", UNSIGNED_8BITS);
-//		dumpInt(fp, "_level1OpHelper1KeyMaskTable", 0x3E888, 112, "%d", UNSIGNED_8BITS);
-//		dumpInt(fp, "_andyMoveTable35", 0x40358, 48, "%3d", SIGNED_8BITS);
-//		dumpInt(fp, "_actionDirectionKeyMaskTable", 0x3E3D4, 352, "0x%02X", UNSIGNED_8BITS);
+			if (version == FULL_FR_10) {
 
-//		dumpInt(fp, "_dbVolumeTable", 0x40010, 129, "0x%02X", UNSIGNED_8BITS);
+				dumpCheckpointData(fp, "_rock_checkpointData", 0x53060, 96);
+//				dumpBinary(fp, "_level1UpdateData2", 0x530C0, 56, "0x%02X", UNSIGNED_8BITS);
+				dumpCheckpointData(fp, "_fort_checkpointData", 0x4E940, 60);
+//				dumpBinary(fp, "_level2UpdateData2", 0x58A50, 56, "0x%02X", UNSIGNED_8BITS);
+				dumpCheckpointData(fp, "_pwr1_checkpointData", 0x517A8, 120);
+//				dumpZeroes("_level3UpdateData2", 80, "0x%02X", 0);
+				dumpCheckpointData(fp, "_isld_checkpointData", 0x4F408, 60);
+//				dumpBinary(fp, "_level4UpdateData2", 0x58A88, 56, "0x%02X", UNSIGNED_8BITS);
+				dumpCheckpointData(fp, "_lava_checkpointData", 0x50248, 72);
+//				dumpBinary(fp, "_level5UpdateData2", 0x50290, 40, "0x%02X", UNSIGNED_8BITS);
+				dumpCheckpointData(fp, "_pwr2_checkpointData", 0x51D68, 36);
+//				dumpZeroes("_level6UpdateData2", 32, "0x%02X", 0);
+				dumpCheckpointData(fp, "_lar1_checkpointData", 0x4F6F8, 108);
+//				dumpBinary(fp, "_level7UpdateData2", 0x4F768, 56, "0x%02X", UNSIGNED_8BITS);
+				dumpCheckpointData(fp, "_lar2_checkpointData", 0x4F8C0, 144);
+//				dumpBinary(fp, "_level8UpdateData2", 0x58B28, 64, "0x%02X", UNSIGNED_8BITS);
+				dumpCheckpointData(fp, "_dark_checkpointData", 0x4E850, 12);
+//				dumpBinary(fp, "_level9UpdateData2", 0x58A40, 8, "0x%02X", UNSIGNED_8BITS);
 
-		dumpFontCharMap(fp, "_fontCharactersTable", 0x3FE88, 39);
-
-	// retail
-
-		dumpCheckpointData(fp, "_rock_checkpointData", 0x53060, 96);
-//		dumpInt(fp, "_level1UpdateData2", 0x530C0, 56, "0x%02X", UNSIGNED_8BITS);
-		dumpCheckpointData(fp, "_fort_checkpointData", 0x4E940, 60);
-//		dumpInt(fp, "_level2UpdateData2", 0x58A50, 56, "0x%02X", UNSIGNED_8BITS);
-		dumpCheckpointData(fp, "_pwr1_checkpointData", 0x517A8, 120);
-//		fillInt("_level3UpdateData2", 80, "0x%02X", 0);
-		dumpCheckpointData(fp, "_isld_checkpointData", 0x4F408, 60);
-//		dumpInt(fp, "_level4UpdateData2", 0x58A88, 56, "0x%02X", UNSIGNED_8BITS);
-		dumpCheckpointData(fp, "_lava_checkpointData", 0x50248, 72);
-//		dumpInt(fp, "_level5UpdateData2", 0x50290, 40, "0x%02X", UNSIGNED_8BITS);
-		dumpCheckpointData(fp, "_pwr2_checkpointData", 0x51D68, 36);
-//		fillInt("_level6UpdateData2", 32, "0x%02X", 0);
-		dumpCheckpointData(fp, "_lar1_checkpointData", 0x4F6F8, 108);
-//		dumpInt(fp, "_level7UpdateData2", 0x4F768, 56, "0x%02X", UNSIGNED_8BITS);
-		dumpCheckpointData(fp, "_lar2_checkpointData", 0x4F8C0, 144);
-//		dumpInt(fp, "_level8UpdateData2", 0x58B28, 64, "0x%02X", UNSIGNED_8BITS);
-		dumpCheckpointData(fp, "_dark_checkpointData", 0x4E850, 12);
-//		dumpInt(fp, "_level9UpdateData2", 0x58A40, 8, "0x%02X", UNSIGNED_8BITS);
-
-		dumpInt(fp, "Game::_transformBufferData1", 0x51DA8, 4536, "0x%02X", UNSIGNED_8BITS, 0x4545A8);
-		dumpInt(fp, "Game::_transformBufferData2", 0x50500, 2152, "0x%02X", UNSIGNED_8BITS, 0x452D00);
-		dumpInt(fp, "byte_452898", 0x50098, 52, "0x%02X", UNSIGNED_8BITS, 0);
-		dumpInt(fp, "byte_4528D0", 0x500D0, 40, "0x%02X", UNSIGNED_8BITS, 0);
-		dumpInt(fp, "byte_4528F8", 0x500F8, 17, "0x%02X", UNSIGNED_8BITS, 0);
-		dumpInt(fp, "_lar2_lutData", 0x50109, 39, "0x%02X", UNSIGNED_8BITS, 0);
-		dumpRect(fp, "_lar1_unkData2", 0x4FB58, 24);
-		dumpInt(fp, "_lar1_unkData3", 0x4FCD8, 96, "0x%02X", UNSIGNED_8BITS, 0);
-		dumpRect(fp, "_lar2_unkData2", 0x4FFC8, 13);
-		dumpInt(fp, "_lar2_unkData3", 0x50098, 52, "0x%02X", UNSIGNED_8BITS, 0);
-
-		fclose(fp);
+				dumpBinary(fp, "Game::_transformBufferData1", 0x51DA8, 4536, "0x%02X", UNSIGNED_8BITS, 0x4545A8);
+				dumpBinary(fp, "Game::_transformBufferData2", 0x50500, 2152, "0x%02X", UNSIGNED_8BITS, 0x452D00);
+				dumpBinary(fp, "byte_452898", 0x50098, 52, "0x%02X", UNSIGNED_8BITS, 0);
+				dumpBinary(fp, "byte_4528D0", 0x500D0, 40, "0x%02X", UNSIGNED_8BITS, 0);
+				dumpBinary(fp, "byte_4528F8", 0x500F8, 17, "0x%02X", UNSIGNED_8BITS, 0);
+				dumpBinary(fp, "_lar2_lutData", 0x50109, 39, "0x%02X", UNSIGNED_8BITS, 0);
+				dumpRect(fp, "_lar1_unkData2", 0x4FB58, 24);
+				dumpBinary(fp, "_lar1_unkData3", 0x4FCD8, 96, "0x%02X", UNSIGNED_8BITS, 0);
+				dumpRect(fp, "_lar2_unkData2", 0x4FFC8, 13);
+				dumpBinary(fp, "_lar2_unkData3", 0x50098, 52, "0x%02X", UNSIGNED_8BITS, 0);
+			}
+			fclose(fp);
+		}
 	}
 	return 0;
 }
