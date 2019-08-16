4385d2dfb972c4303525b43db05636da51cc5237
Gregory Montoir <cyx@users.sourceforge.net>
2019-08-16 22:38:53 +0800

    tools/hod_decoder: add build_web_zip.py

---
+++ tools/hod_decoder/build_web_zip.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import os
+import re
+import subprocess
+import zipfile
+
+ARCHIVE = 'hod_decoder-%s.zip'
+
+SRCS = [
+	'Makefile',
+	'.*\.h$',
+	'.*cpp$',
+]
+
+BINS = [
+	'hod_decoder.exe',
+	'offs_00000001.txt',
+]
+
+VERSION = subprocess.check_output('git rev-parse --short HEAD'.split()).strip()
+print VERSION
+
+name = ARCHIVE % VERSION
+zf = zipfile.ZipFile(name, 'w')
+src_dir = os.listdir('.')
+for pattern in BINS:
+	for filename in src_dir:
+		if re.match(pattern, filename):
+			zf.write(filename)
+for pattern in SRCS:
+	for filename in src_dir:
+		if re.match(pattern, filename):
+			arcname = 'src/' + filename
+			zf.write(filename, arcname)
+zf.close()
--- tools/hod_decoder/make-mingw.sh
+++ tools/hod_decoder/make-mingw.sh
@@ -3,6 +3,6 @@ set -x
 ZLIB=$HOME/Install/zlib-1.2.11
 
 OUT=hod_decoder.exe
-i686-w64-mingw32-g++ -o $OUT decode_mst.cpp fileio.cpp lzw.cpp main.cpp screenshot.cpp wav.cpp -L $ZLIB/ -I $ZLIB/ -lz -static-libgcc -static-libstdc++
+i686-w64-mingw32-g++ -o $OUT decode_sss.cpp decode_mst.cpp fileio.cpp lzw.cpp main.cpp screenshot.cpp wav.cpp -L $ZLIB/ -I $ZLIB/ -lz -static-libgcc -static-libstdc++
 i686-w64-mingw32-strip $OUT
 upx -9 $OUT
