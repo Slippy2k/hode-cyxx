82c8886994da7bf288707ce385b0d3b6cb5b47e6
Gregory Montoir <cyx@users.sourceforge.net>
2019-09-05 10:39:43 +0800

    web: get version from README.txt

--- web/package_web.py
+++ web/package_web.py
@@ -3,8 +3,6 @@
 import os, re, tarfile, shutil
 import zipfile
 
-VERSION = '0.1.9'
-
 SRC_DIR = '..'
 
 SRCS = (
@@ -21,19 +19,30 @@ BINS = (
 	'README.txt',
 )
 
+def getVersion():
+	f = file(os.path.join(SRC_DIR, 'README.txt'))
+	for line in f.readlines():
+		m = re.search('Release version: (\d+\.\d+\.\d+)', line.strip())
+		if m:
+			return m.group(1)
+	return None
+
+version = getVersion()
+assert version
+
 src_dir = os.listdir(SRC_DIR)
 
-filename = 'hode-%s.tar.bz2' % VERSION
+filename = 'hode-%s.tar.bz2' % version
 tf = tarfile.open(filename, 'w:bz2')
 for name in SRCS:
 	for filename in src_dir:
 		if re.match(name, filename):
 			entry_path = os.path.join(SRC_DIR, filename)
-			entry_name = 'hode-' + VERSION + '/' + filename
+			entry_name = 'hode-' + version + '/' + filename
 			tf.add(entry_path, arcname=entry_name)
 tf.close()
 
-filename = 'hode-%s.zip' % VERSION
+filename = 'hode-%s-win32.zip' % version
 zf = zipfile.ZipFile(filename, 'w', zipfile.ZIP_DEFLATED)
 for name in BINS:
 	for filename in src_dir:
