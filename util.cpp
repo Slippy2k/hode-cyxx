aff7c8b282bd02d64f6f7a5a02f57eb90a2d4239
Gregory Montoir <cyx@users.sourceforge.net>
2019-09-15 21:33:46 +0800

    hode: add email address to source code files

--- util.cpp
+++ util.cpp
@@ -1,6 +1,6 @@
 /*
  * Heart Of Darkness engine rewrite
- * Copyright (C) 2009-2011 Gregory Montoir
+ * Copyright (C) 2009-2011 Gregory Montoir (cyx@users.sourceforge.net)
  */
 
 #ifdef __ANDROID__
