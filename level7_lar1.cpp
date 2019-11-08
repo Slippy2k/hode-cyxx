24ef3d7d53a96361142051125633372208be7d71
Gregory Montoir <cyx@users.sourceforge.net>
2019-11-08 16:23:01 +0800

    hode: fix early _checkpoint 2 in screen 5 'lar1'

--- level7_lar1.cpp
+++ level7_lar1.cpp
@@ -223,7 +223,6 @@ void Level_lar1::postScreenUpdate_lar1_screen5() {
 	_g->updateGatesLar(o3, &_lar1_gatesData[3 * 4], 3);
 	if (_res->_currentScreenResourceNum == 5) {
 		if (_checkpoint >= 1 && _checkpoint <= 3) {
-			_checkpoint = 2;
 			BoundingBox b = { 194, 0, 255, 88 };
 			AndyLvlObjectData *data = (AndyLvlObjectData *)_g->getLvlObjectDataPtr(_andyObject, kObjectDataTypeAndy);
 			if (_g->clipBoundingBox(&b, &data->boundingBox) && (_lar1_gatesData[0x18] & 0xF0) == 0x10) {
