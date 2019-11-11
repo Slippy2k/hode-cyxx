4d8ca8aa7d0feb1a4cbab8b8a6342091c44dabfd
Gregory Montoir <cyx@users.sourceforge.net>
2019-11-11 21:37:49 +0800

    tools/datdec: remove code obsoleted by hod_decoder

--- tools/transform/dump.cpp
+++ tools/transform/dump.cpp
@@ -21,12 +21,8 @@ int main(int argc, char *argv[]) {
 	if (argc == 2) {
 		FILE *fp = fopen(argv[1], "rb");
 		if (fp) {
-//			DumpData(fp, 0x3D960, 2152, "byte_43D960");
-//			DumpData(fp, 0x3EA78, 4536, "byte_43EA78");
-
-DumpData(fp, 0x3FD40, 96, "_game_level0_updateData1");
-DumpData(fp, 0x3FDA0, 56, "_game_level0_updateData2");
-
+			DumpData(fp, 0x3D960, 2152, "byte_43D960");
+			DumpData(fp, 0x3EA78, 4536, "byte_43EA78");
 			fclose(fp);
 		}
 	}
