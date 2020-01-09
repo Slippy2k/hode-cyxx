48202f7fbb6c7e8ad2065c6d7148c7ec690a4112
Gregory Montoir <cyx@users.sourceforge.net>
2020-01-09 18:08:03 +0800

    tools: add Japanese release information

--- tools/dump_release_info.sh
+++ tools/dump_release_info.sh
@@ -2,6 +2,7 @@
 echo "# This file lists released versions of 'Heart of Darkness' from Amazing Studio"
 
 python dump_release_info.py "Heart of Darkness French (Win32)"                   $HOME/Data/heart_of_darkness/DATA_fr/
+python dump_release_info.py "Heart of Darkness Japanese (Win32)"                 $HOME/Data/heart_of_darkness/DATA_jp/
 python dump_release_info.py "Heart of Darkness Demo V1.2 (Win32)"                $HOME/Data/heart_of_darkness/DATA_demo/
 python dump_release_info.py "Heart of Darkness Mini-Game V1.3 Kellogg's (Win32)" $HOME/Data/heart_of_darkness/DATA_Kellogs/
 python dump_release_info.py "Heart of Darkness Coca Cola Edition (Win32)"        $HOME/Data/heart_of_darkness/DATA_CocaCola/
