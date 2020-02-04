003d56db8c3df477880e506708bc56d7102cf159
Gregory Montoir <cyx@users.sourceforge.net>
2020-02-04 09:27:16 +0800

    tools/decode_dem.py: add parsing for 'REC2' (PSX)

--- tools/decode_hod_dem.py
+++ tools/decode_hod_dem.py
@@ -9,16 +9,29 @@ while True:
 	signature = f.read(4)
 	if not signature:
 		break
-	assert signature == 'REC1'
-	total_size    = struct.unpack('<I', f.read(4))[0]
-	random_seed   = struct.unpack('<I', f.read(4))[0]
-	keymask_size  = struct.unpack('<I', f.read(4))[0]
-	level         = ord(f.read(1))
-	checkpoint    = ord(f.read(1))
-	difficulty    = ord(f.read(1))
-	shuffle_count = ord(f.read(1)) # _rnd.initTable
-	f.read(4) # ptr to actionKeyMask
-	f.read(4) # ptr to directionKeyMask
+	if signature == 'REC1':
+		total_size    = struct.unpack('<I', f.read(4))[0]
+		random_seed   = struct.unpack('<I', f.read(4))[0]
+		keymask_size  = struct.unpack('<I', f.read(4))[0]
+		level         = ord(f.read(1))
+		checkpoint    = ord(f.read(1))
+		difficulty    = ord(f.read(1))
+		shuffle_count = ord(f.read(1)) # _rnd.initTable
+		f.read(4) # ptr to actionKeyMask
+		f.read(4) # ptr to directionKeyMask
+		assert keymask_size * 2 == total_size - 8 - 20
+		f.seek(keymask_size * 2, os.SEEK_CUR)
+	else: # 'REC2'
+		random_seed   = struct.unpack('<I', signature)[0]
+		keymask_size  = struct.unpack('<I', f.read(4))[0]
+		level         = ord(f.read(1))
+		checkpoint    = ord(f.read(1))
+		difficulty    = ord(f.read(1))
+		shuffle_count = ord(f.read(1)) # _rnd.initTable
+		f.read(4) # ptr to actionKeyMask
+		f.read(4) # ptr to directionKeyMask
+		signature = f.read(4)
+		assert signature == 'REC2'
+		f.seek(0x1000 - 0x18, os.SEEK_CUR)
+		total_size = keymask_size * 2
 	print 'rand 0x%x size %d tblsize %d level %d checkpoint %d' % (random_seed, total_size, keymask_size, level, checkpoint)
-	assert keymask_size * 2 == total_size - 8 - 20
-	f.seek(keymask_size * 2, os.SEEK_CUR)
