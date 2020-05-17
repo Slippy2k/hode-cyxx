120841128e69599dbc53cd8d5e20d05b4d492fcd
Gregory Montoir <cyx@users.sourceforge.net>
2020-05-17 13:05:06 +0800

    tools/dump_release_info: add German release information

--- tools/dump_release_info.sh
+++ tools/dump_release_info.sh
@@ -2,6 +2,7 @@
 echo "# This file lists released versions of 'Heart of Darkness' from Amazing Studio"
 
 python dump_release_info.py "Heart of Darkness French (Win32)"                   $HOME/Data/heart_of_darkness/DATA_fr/
+python dump_release_info.py "Heart of Darkness German (Win32)"                   $HOME/Data/heart_of_darkness/DATA_gr/
 python dump_release_info.py "Heart of Darkness Italian (Win32)"                  $HOME/Data/heart_of_darkness/DATA_it/
 python dump_release_info.py "Heart of Darkness Japanese (Win32)"                 $HOME/Data/heart_of_darkness/DATA_jp/
 python dump_release_info.py "Heart of Darkness Demo V1.0 (Win32)"                $HOME/Data/heart_of_darkness/DATA_demo_v10/
