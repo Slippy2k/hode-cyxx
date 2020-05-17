105ad2d7ba653bbcbf18cebfbfce8705c4fe88ae
Gregory Montoir <cyx@users.sourceforge.net>
2020-05-17 10:23:50 +0800

    web: set time and date for zipfile

--- web/package_web.py
+++ web/package_web.py
@@ -53,8 +53,8 @@ def getVersion():
 version = getVersion()
 assert version
 
-t = datetime.date.today().timetuple()
-timestamp = time.mktime(t)
+timetuple = datetime.date.today().timetuple()
+timestamp = time.mktime(timetuple)
 
 X86_REGS = { '_eax':'va', '_ebx':'vb', '_ecx':'vc', '_edx':'vd', '_ebp':'ve', '_edi':'vf', '_esi':'vg' }
 
@@ -69,7 +69,7 @@ def tidyCpp(fname):
 		if line.startswith('// 4'):
 			continue
 		# strip comments with x86 registers
-		ndx = line.find('// _e')
+		ndx = line.find(' // _e')
 		if ndx != -1:
 			line = line[:ndx] + '\n'
 		# replace variables named after x86 registers
@@ -85,24 +85,32 @@ def tidyCpp(fname):
 
 src_dir = os.listdir(SRC_DIR)
 
+def getTarInfo(tf, path, entryname, timestamp):
+	fileobj = tidyCpp(path)
+	tarinfo = tf.gettarinfo(name=entryname, arcname=entryname, fileobj=fileobj)
+	tarinfo.mtime = timestamp
+	fileobj.seek(0)
+	return (tarinfo, fileobj)
+
 filename = 'hode-%s.tar.bz2' % version
 prefix = 'hode-%s/' % version
 tf = tarfile.open(filename, 'w:bz2')
 for name in SRC:
 	for filename in src_dir:
 		if re.match(name, filename):
-			entryname = prefix + filename
-			entrypath = os.path.join(SRC_DIR, filename)
-			fileobj = tidyCpp(entrypath)
-			tarinfo = tf.gettarinfo(name=filename, arcname=entryname, fileobj=fileobj)
-			tarinfo.mtime = timestamp
-			fileobj.seek(0)
-			tf.addfile(tarinfo, fileobj)
+			args = getTarInfo(tf, os.path.join(SRC_DIR, filename), prefix + filename, timestamp)
+			tf.addfile(*args)
 	for dirname in EXTRA_DIRS:
 		path = os.path.join(dirname, name)
 		if os.path.exists(path):
-			tf.add(path, prefix + name)
+			args = getTarInfo(tf, path, prefix + name, timestamp)
+			tf.addfile(*args)
 tf.close()
+
+def getZipDataStr(zf, path, name, t):
+	zi = zipfile.ZipInfo(name)
+	zi.date_time = t[0:6]
+	return (zi, file(path).read())
 
 zipfiles = [ ('hode-%s-win32.zip', BIN_WIN32), ('hode-%s-psp.zip', BIN_PSP) ]
 for f in zipfiles:
@@ -111,11 +119,13 @@ for f in zipfiles:
 	for name in f[1]:
 		for filename in src_dir:
 			if re.match(name, filename):
-				zf.write(os.path.join(SRC_DIR, filename), filename)
+				args = getZipDataStr(zf, os.path.join(SRC_DIR, filename), filename, timetuple)
+				zf.writestr(*args, compress_type=zipfile.ZIP_DEFLATED)
 		for dirname in EXTRA_DIRS:
 			path = os.path.join(dirname, name)
 			if os.path.exists(path):
-				zf.write(path, name)
+				args = getZipDataStr(zf, path, name, timetuple)
+				zf.writestr(*args, compress_type=zipfile.ZIP_DEFLATED)
 	zf.close()
 
 filename = 'hode-%s-wii.zip' % version
