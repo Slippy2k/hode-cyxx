f30ea532c7e30b74b713b40d02148980aeddc9b4
Gregory Montoir <cyx@users.sourceforge.net>
2020-12-19 15:39:12 +0800

    hode: add The Adventure Begins V1.3 demo

--- tools/dump_release_info.sh
+++ tools/dump_release_info.sh
@@ -1,14 +1,15 @@
 
 echo "# This file lists released versions of 'Heart of Darkness' from Amazing Studio"
 
-python dump_release_info.py "Heart of Darkness French (Win32)"                   $HOME/Data/heart_of_darkness/DATA_fr/
-python dump_release_info.py "Heart of Darkness German (Win32)"                   $HOME/Data/heart_of_darkness/DATA_gr/
-python dump_release_info.py "Heart of Darkness Italian (Win32)"                  $HOME/Data/heart_of_darkness/DATA_it/
-python dump_release_info.py "Heart of Darkness Japanese (Win32)"                 $HOME/Data/heart_of_darkness/DATA_jp/
-python dump_release_info.py "Heart of Darkness Demo V1.0 (Win32)"                $HOME/Data/heart_of_darkness/DATA_demo_v10/
-python dump_release_info.py "Heart of Darkness Demo V1.2 (Win32)"                $HOME/Data/heart_of_darkness/DATA_demo_v12/
-python dump_release_info.py "Heart of Darkness Mini-Game V1.3 Kellogg's (Win32)" $HOME/Data/heart_of_darkness/DATA_Kellogs/
-python dump_release_info.py "Heart of Darkness Coca Cola Edition (Win32)"        $HOME/Data/heart_of_darkness/DATA_CocaCola/
+python dump_release_info.py "Heart of Darkness French (Win32)"                    $HOME/Data/heart_of_darkness/DATA_fr/
+python dump_release_info.py "Heart of Darkness German (Win32)"                    $HOME/Data/heart_of_darkness/DATA_gr/
+python dump_release_info.py "Heart of Darkness Italian (Win32)"                   $HOME/Data/heart_of_darkness/DATA_it/
+python dump_release_info.py "Heart of Darkness Japanese (Win32)"                  $HOME/Data/heart_of_darkness/DATA_jp/
+python dump_release_info.py "Heart of Darkness Demo V1.0 (Win32)"                 $HOME/Data/heart_of_darkness/DATA_demo_v10/
+python dump_release_info.py "Heart of Darkness Demo V1.2 (Win32)"                 $HOME/Data/heart_of_darkness/DATA_demo_v12/
+python dump_release_info.py "Heart of Darkness Mini-Game V1.3 Kellogg's (Win32)"  $HOME/Data/heart_of_darkness/DATA_Kellogs/
+python dump_release_info.py "Heart of Darkness The Adventure Begins V1.3 (Win32)" $HOME/Data/heart_of_darkness/DATA_demo_adv/
+python dump_release_info.py "Heart of Darkness Coca Cola Edition (Win32)"         $HOME/Data/heart_of_darkness/DATA_CocaCola/
 
-python dump_release_info.py "Heart of Darkness Demo (PSX)"                       $HOME/Data/heart_of_darkness/DATA_psx_demo/
-python dump_release_info.py "Heart of Darkness NTSC-U (PSX)"                     $HOME/Data/heart_of_darkness/DATA_psx/
+python dump_release_info.py "Heart of Darkness Demo (PSX)"                        $HOME/Data/heart_of_darkness/DATA_psx_demo/
+python dump_release_info.py "Heart of Darkness NTSC-U (PSX)"                      $HOME/Data/heart_of_darkness/DATA_psx/
