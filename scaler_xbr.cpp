7960f1ef35ed0af564fb40a3fae492f7699a6e13
Gregory Montoir <cyx@users.sourceforge.net>
2021-02-14 21:41:53 +0800

    hode: xbr - add 18 bits RGB YUV lookup table

--- scaler_xbr.cpp
+++ scaler_xbr.cpp
@@ -1,7 +1,15 @@
 
 #include "scaler.h"
 
-static uint32_t pixel_diff(uint32_t x, uint32_t y) {
+static bool _rgb_yuv_init;
+static uint32_t _rgb_yuv[1 << 18];
+
+static uint32_t yuv_diff(uint32_t x, uint32_t y) {
+	if (1) {
+		const int yuv1 = _rgb_yuv[((x >> 2) & 0x3F) | ((x >> 4) & (0x3F << 6)) | ((x >> 6) & (0x3F << 12))];
+		const int yuv2 = _rgb_yuv[((y >> 2) & 0x3F) | ((y >> 4) & (0x3F << 6)) | ((y >> 6) & (0x3F << 12))];
+		return ABS((yuv1 >> 16) - (yuv2 >> 16)) + ABS(((yuv1 >> 8) & 255) - ((yuv2 >> 8) & 255)) + ABS((yuv1 & 255) - (yuv2 & 255));
+	}
 	int r, g, b;
 	r = (x >> 16) & 255;
 	g = (x >>  8) & 255;
@@ -15,32 +23,30 @@ static uint32_t pixel_diff(uint32_t x, uint32_t y) {
 	int y2 = ( 299 * r + 587 * g + 114 * b) / 1000;
 	int u2 = (-169 * r - 331 * g + 500 * b) / 1000 + 128;
 	int v2 = ( 500 * r - 419 * g -  81 * b) / 1000 + 128;
-	return abs(y1 - y2) + abs(u1 - u2) + abs(v1 - v2);
+	return ABS(y1 - y2) + ABS(u1 - u2) + ABS(v1 - v2);
 }
 
 template <int M, int S>
 static uint32_t interpolate(uint32_t a, uint32_t b) {
-	static const uint32_t kMask1 = 0xFF00FF; // red blue
-	static const uint32_t kMask2 = 0x00FF00; // green
+	static const uint32_t kMask = 0xFF00FF;
 
-	uint32_t r;
-	r  = (((((b & kMask1) - (a & kMask1)) * M) >> S) + (a & kMask1)) & kMask1;
-	r |= (((((b & kMask2) - (a & kMask2)) * M) >> S) + (a & kMask2)) & kMask2;
-	return r;
+	const uint32_t m1 = (((((b & kMask) - (a & kMask)) * M) >> S) + (a & kMask)) & kMask;
+	a >>= 8;
+	b >>= 8;
+	const uint32_t m2 = (((((b & kMask) - (a & kMask)) * M) >> S) + (a & kMask)) & kMask;
+	return m1 | (m2 << 8);
 }
 
-#define ALPHA_BLEND_BASE(a, b, m, s) interpolate<m,s>(a, b)
+#define ALPHA_BLEND_32_W(a, b)  interpolate<1,3>(a, b)
+#define ALPHA_BLEND_64_W(a, b)  interpolate<1,2>(a, b)
+#define ALPHA_BLEND_128_W(a, b) interpolate<1,1>(a, b)
+#define ALPHA_BLEND_192_W(a, b) interpolate<3,2>(a, b)
+#define ALPHA_BLEND_224_W(a, b) interpolate<7,3>(a, b)
 
-#define ALPHA_BLEND_32_W(a, b)  ALPHA_BLEND_BASE(a, b, 1, 3)
-#define ALPHA_BLEND_64_W(a, b)  ALPHA_BLEND_BASE(a, b, 1, 2)
-#define ALPHA_BLEND_128_W(a, b) ALPHA_BLEND_BASE(a, b, 1, 1)
-#define ALPHA_BLEND_192_W(a, b) ALPHA_BLEND_BASE(a, b, 3, 2)
-#define ALPHA_BLEND_224_W(a, b) ALPHA_BLEND_BASE(a, b, 7, 3)
-
-#define df(A, B) pixel_diff(A, B)
+#define df(A, B) yuv_diff(A, B)
 #define eq(A, B) (df(A, B) < 155)
 
-#define FILT2(PE, PI, PH, PF, PG, PC, PD, PB, PA, G5, C4, G0, D0, C1, B1, F4, I4, H5, I5, A0, A1, N0, N1, N2, N3) do { \
+#define filt2b(PE, PI, PH, PF, PG, PC, PD, PB, PA, G5, C4, G0, D0, C1, B1, F4, I4, H5, I5, A0, A1, N0, N1, N2, N3) do { \
     if (PE != PH && PE != PF) {                                                                     \
         const unsigned e = df(PE,PC) + df(PE,PG) + df(PI,H5) + df(PI,F4) + (df(PH,PF)<<2);          \
         const unsigned i = df(PH,PD) + df(PH,I5) + df(PF,I4) + df(PF,PB) + (df(PE,PI)<<2);          \
@@ -61,7 +67,7 @@ static uint32_t interpolate(uint32_t a, uint32_t b) {
                 } else if (up) {                                                                    \
                     E[N3] = ALPHA_BLEND_192_W(E[N3], px);                                           \
                     E[N1] = ALPHA_BLEND_64_W( E[N1], px);                                           \
-                } else { /* diagonal */                                                             \
+                } else {                                                                            \
                     E[N3] = ALPHA_BLEND_128_W(E[N3], px);                                           \
                 }                                                                                   \
             } else {                                                                                \
@@ -71,7 +77,7 @@ static uint32_t interpolate(uint32_t a, uint32_t b) {
     }                                                                                               \
 } while (0)
 
-#define FILT3(PE, PI, PH, PF, PG, PC, PD, PB, PA, G5, C4, G0, D0, C1, B1, F4, I4, H5, I5, A0, A1, N0, N1, N2, N3, N4, N5, N6, N7, N8) do { \
+#define filt3a(PE, PI, PH, PF, PG, PC, PD, PB, PA, G5, C4, G0, D0, C1, B1, F4, I4, H5, I5, A0, A1, N0, N1, N2, N3, N4, N5, N6, N7, N8) do { \
     if (PE != PH && PE != PF) {                                                                     \
         const unsigned e = df(PE,PC) + df(PE,PG) + df(PI,H5) + df(PI,F4) + (df(PH,PF)<<2);          \
         const unsigned i = df(PH,PD) + df(PH,I5) + df(PF,I4) + df(PF,PB) + (df(PE,PI)<<2);          \
@@ -98,7 +104,7 @@ static uint32_t interpolate(uint32_t a, uint32_t b) {
                     E[N7] = ALPHA_BLEND_64_W( E[N7], px);                                           \
                     E[N2] = ALPHA_BLEND_64_W( E[N2], px);                                           \
                     E[N8] = px;                                                                     \
-                } else { /* diagonal */                                                             \
+                } else {                                                                            \
                     E[N8] = ALPHA_BLEND_224_W(E[N8], px);                                           \
                     E[N5] = ALPHA_BLEND_32_W( E[N5], px);                                           \
                     E[N7] = ALPHA_BLEND_32_W( E[N7], px);                                           \
@@ -110,7 +116,7 @@ static uint32_t interpolate(uint32_t a, uint32_t b) {
     }                                                                                               \
 } while (0)
 
-#define FILT4(PE, PI, PH, PF, PG, PC, PD, PB, PA, G5, C4, G0, D0, C1, B1, F4, I4, H5, I5, A0, A1, N15, N14, N11, N3, N7, N10, N13, N12, N9, N6, N2, N1, N5, N8, N4, N0) do { \
+#define filt4b(PE, PI, PH, PF, PG, PC, PD, PB, PA, G5, C4, G0, D0, C1, B1, F4, I4, H5, I5, A0, A1, N15, N14, N11, N3, N7, N10, N13, N12, N9, N6, N2, N1, N5, N8, N4, N0) do { \
     if (PE != PH && PE != PF) {                                                                     \
         const unsigned e = df(PE,PC) + df(PE,PG) + df(PI,H5) + df(PI,F4) + (df(PH,PF)<<2);          \
         const unsigned i = df(PH,PD) + df(PH,I5) + df(PF,I4) + df(PF,PB) + (df(PE,PI)<<2);          \
@@ -141,7 +147,7 @@ static uint32_t interpolate(uint32_t a, uint32_t b) {
                     E[N3 ] = ALPHA_BLEND_64_W( E[N3 ], px);                                         \
                     E[N11] = px;                                                                    \
                     E[N15] = px;                                                                    \
-                } else { /* diagonal */                                                             \
+                } else {                                                                            \
                     E[N11] = ALPHA_BLEND_128_W(E[N11], px);                                         \
                     E[N14] = ALPHA_BLEND_128_W(E[N14], px);                                         \
                     E[N15] = px;                                                                    \
@@ -155,14 +161,14 @@ static uint32_t interpolate(uint32_t a, uint32_t b) {
 
 template <int N>
 static void xbr_filter(uint32_t *dst, int dstPitch, const uint32_t *src, int srcPitch, int w, int h) {
-    const int nl = dstPitch;
-    const int nl1 = nl + nl;
-    const int nl2 = nl1 + nl;
+	const int nl = dstPitch;
+	const int nl1 = nl + nl;
+	const int nl2 = nl1 + nl;
 
-    for (int y = 0; y < h; y++) {
+	for (int y = 0; y < h; ++y) {
 
-        uint32_t *E = dst + y * dstPitch * N;
-        const uint32_t *sa2 = src + y * srcPitch - 8 / 4; /* center */
+		uint32_t *E = dst + y * dstPitch * N;
+        const uint32_t *sa2 = src + y * srcPitch - 2; /* center */
         const uint32_t *sa1 = sa2 - srcPitch; /* up x1 */
         const uint32_t *sa0 = sa1 - srcPitch; /* up x2 */
         const uint32_t *sa3 = sa2 + srcPitch; /* down x1 */
@@ -217,43 +223,56 @@ static void xbr_filter(uint32_t *dst, int dstPitch, const uint32_t *src, int src
                 E[0]  = E[1]      =     // 0, 1
                 E[nl] = E[nl + 1] = PE; // 2, 3
 
-                FILT2(PE, PI, PH, PF, PG, PC, PD, PB, PA, G5, C4, G0, D0, C1, B1, F4, I4, H5, I5, A0, A1, 0, 1, nl, nl+1);
-                FILT2(PE, PC, PF, PB, PI, PA, PH, PD, PG, I4, A1, I5, H5, A0, D0, B1, C1, F4, C4, G5, G0, nl, 0, nl+1, 1);
-                FILT2(PE, PA, PB, PD, PC, PG, PF, PH, PI, C1, G0, C4, F4, G5, H5, D0, A0, B1, A1, I4, I5, nl+1, nl, 1, 0);
-                FILT2(PE, PG, PD, PH, PA, PI, PB, PF, PC, A0, I5, A1, B1, I4, F4, H5, G5, D0, G0, C1, C4, 1, nl+1, 0, nl);
+                filt2b(PE, PI, PH, PF, PG, PC, PD, PB, PA, G5, C4, G0, D0, C1, B1, F4, I4, H5, I5, A0, A1, 0, 1, nl, nl+1);
+                filt2b(PE, PC, PF, PB, PI, PA, PH, PD, PG, I4, A1, I5, H5, A0, D0, B1, C1, F4, C4, G5, G0, nl, 0, nl+1, 1);
+                filt2b(PE, PA, PB, PD, PC, PG, PF, PH, PI, C1, G0, C4, F4, G5, H5, D0, A0, B1, A1, I4, I5, nl+1, nl, 1, 0);
+                filt2b(PE, PG, PD, PH, PA, PI, PB, PF, PC, A0, I5, A1, B1, I4, F4, H5, G5, D0, G0, C1, C4, 1, nl+1, 0, nl);
             } else if (N == 3) {
                 E[0]   = E[1]     = E[2]     =     // 0, 1, 2
                 E[nl]  = E[nl+1]  = E[nl+2]  =     // 3, 4, 5
                 E[nl1] = E[nl1+1] = E[nl1+2] = PE; // 6, 7, 8
 
-                FILT3(PE, PI, PH, PF, PG, PC, PD, PB, PA, G5, C4, G0, D0, C1, B1, F4, I4, H5, I5, A0, A1, 0, 1, 2, nl, nl+1, nl+2, nl1, nl1+1, nl1+2);
-                FILT3(PE, PC, PF, PB, PI, PA, PH, PD, PG, I4, A1, I5, H5, A0, D0, B1, C1, F4, C4, G5, G0, nl1, nl, 0, nl1+1, nl+1, 1, nl1+2, nl+2, 2);
-                FILT3(PE, PA, PB, PD, PC, PG, PF, PH, PI, C1, G0, C4, F4, G5, H5, D0, A0, B1, A1, I4, I5, nl1+2, nl1+1, nl1, nl+2, nl+1, nl, 2, 1, 0);
-                FILT3(PE, PG, PD, PH, PA, PI, PB, PF, PC, A0, I5, A1, B1, I4, F4, H5, G5, D0, G0, C1, C4, 2, nl+2, nl1+2, 1, nl+1, nl1+1, 0, nl, nl1);
+                filt3a(PE, PI, PH, PF, PG, PC, PD, PB, PA, G5, C4, G0, D0, C1, B1, F4, I4, H5, I5, A0, A1, 0, 1, 2, nl, nl+1, nl+2, nl1, nl1+1, nl1+2);
+                filt3a(PE, PC, PF, PB, PI, PA, PH, PD, PG, I4, A1, I5, H5, A0, D0, B1, C1, F4, C4, G5, G0, nl1, nl, 0, nl1+1, nl+1, 1, nl1+2, nl+2, 2);
+                filt3a(PE, PA, PB, PD, PC, PG, PF, PH, PI, C1, G0, C4, F4, G5, H5, D0, A0, B1, A1, I4, I5, nl1+2, nl1+1, nl1, nl+2, nl+1, nl, 2, 1, 0);
+                filt3a(PE, PG, PD, PH, PA, PI, PB, PF, PC, A0, I5, A1, B1, I4, F4, H5, G5, D0, G0, C1, C4, 2, nl+2, nl1+2, 1, nl+1, nl1+1, 0, nl, nl1);
             } else if (N == 4) {
                 E[0]   = E[1]     = E[2]     = E[3]     =     //  0,  1,  2,  3
                 E[nl]  = E[nl+1]  = E[nl+2]  = E[nl+3]  =     //  4,  5,  6,  7
                 E[nl1] = E[nl1+1] = E[nl1+2] = E[nl1+3] =     //  8,  9, 10, 11
                 E[nl2] = E[nl2+1] = E[nl2+2] = E[nl2+3] = PE; // 12, 13, 14, 15
 
-                FILT4(PE, PI, PH, PF, PG, PC, PD, PB, PA, G5, C4, G0, D0, C1, B1, F4, I4, H5, I5, A0, A1, nl2+3, nl2+2, nl1+3, 3, nl+3, nl1+2, nl2+1, nl2, nl1+1, nl+2, 2, 1, nl+1, nl1, nl, 0);
-                FILT4(PE, PC, PF, PB, PI, PA, PH, PD, PG, I4, A1, I5, H5, A0, D0, B1, C1, F4, C4, G5, G0, 3, nl+3, 2, 0, 1, nl+2, nl1+3, nl2+3, nl1+2, nl+1, nl, nl1, nl1+1, nl2+2, nl2+1, nl2);
-                FILT4(PE, PA, PB, PD, PC, PG, PF, PH, PI, C1, G0, C4, F4, G5, H5, D0, A0, B1, A1, I4, I5, 0, 1, nl, nl2, nl1, nl+1, 2, 3, nl+2, nl1+1, nl2+1, nl2+2, nl1+2, nl+3, nl1+3, nl2+3);
-                FILT4(PE, PG, PD, PH, PA, PI, PB, PF, PC, A0, I5, A1, B1, I4, F4, H5, G5, D0, G0, C1, C4, nl2, nl1, nl2+1, nl2+3, nl2+2, nl1+1, nl, 0, nl+1, nl1+2, nl1+3, nl+3, nl+2, 1, 2, 3);
+                filt4b(PE, PI, PH, PF, PG, PC, PD, PB, PA, G5, C4, G0, D0, C1, B1, F4, I4, H5, I5, A0, A1, nl2+3, nl2+2, nl1+3, 3, nl+3, nl1+2, nl2+1, nl2, nl1+1, nl+2, 2, 1, nl+1, nl1, nl, 0);
+                filt4b(PE, PC, PF, PB, PI, PA, PH, PD, PG, I4, A1, I5, H5, A0, D0, B1, C1, F4, C4, G5, G0, 3, nl+3, 2, 0, 1, nl+2, nl1+3, nl2+3, nl1+2, nl+1, nl, nl1, nl1+1, nl2+2, nl2+1, nl2);
+                filt4b(PE, PA, PB, PD, PC, PG, PF, PH, PI, C1, G0, C4, F4, G5, H5, D0, A0, B1, A1, I4, I5, 0, 1, nl, nl2, nl1, nl+1, 2, 3, nl+2, nl1+1, nl2+1, nl2+2, nl1+2, nl+3, nl1+3, nl2+3);
+                filt4b(PE, PG, PD, PH, PA, PI, PB, PF, PC, A0, I5, A1, B1, I4, F4, H5, G5, D0, G0, C1, C4, nl2, nl1, nl2+1, nl2+3, nl2+2, nl1+1, nl, 0, nl+1, nl1+2, nl1+3, nl+3, nl+2, 1, 2, 3);
             }
 
-            sa0 += 1;
-            sa1 += 1;
-            sa2 += 1;
-            sa3 += 1;
-            sa4 += 1;
+			++sa0;
+			++sa1;
+			++sa2;
+			++sa3;
+			++sa4;
 
-            E += N;
-        }
-    }
+			E += N;
+		}
+	}
 }
 
 static void scale_xbr(int factor, uint32_t *dst, int dstPitch, const uint32_t *src, int srcPitch, int w, int h) {
+	if (!_rgb_yuv_init) {
+		for (int r = 0; r < 0x40; ++r) {
+			for (int g = 0; g < 0x40; ++g) {
+				for (int b = 0; b < 0x40; ++b) {
+					const int y = ( 299 * r + 587 * g + 114 * b) * 4 / 1000;
+					const int u = (-169 * r - 331 * g + 500 * b) * 4 / 1000 + 128;
+					const int v = ( 500 * r - 419 * g -  81 * b) * 4 / 1000 + 128;
+					_rgb_yuv[(r << 12) | (g << 6) | b] = (y << 16) | (u << 8) | v;
+				}
+			}
+		}
+		_rgb_yuv_init = true;
+	}
 	switch (factor) {
 	case 2:
 		xbr_filter<2>(dst, dstPitch, src, srcPitch, w, h);
