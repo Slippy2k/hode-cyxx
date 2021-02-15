f4321cb2217de2ca24d339a38eef615d63112210
Gregory Montoir <cyx@users.sourceforge.net>
2021-02-15 08:56:56 +0800

    hode: xbr - reduce palette lookups

--- scaler_xbr.cpp
+++ scaler_xbr.cpp
@@ -8,10 +8,8 @@ static uint32_t _rgb_yuv[1 << 18];
 
 static uint8_t _yuv[256 * 3];
 
-static uint32_t yuv_diff(uint32_t x, uint32_t y, const uint8_t *p) {
+static uint32_t yuv_diff(uint32_t x, uint32_t y) {
 	if (1) {
-		x = p[x];
-		y = p[y];
 		const int dy = _yuv[x * 3]     - _yuv[y * 3];
 		const int du = _yuv[x * 3 + 1] - _yuv[y * 3 + 1];
 		const int dv = _yuv[x * 3 + 2] - _yuv[y * 3 + 2];
@@ -49,7 +47,7 @@ static uint32_t interpolate(uint32_t a, uint32_t b) {
 	return m1 | (m2 << 8);
 }
 
-#define COLOR(x) palette[src[x]]
+#define COLOR(x) palette[x]
 
 #define ALPHA_BLEND_32_W(a, b)  interpolate<1,3>(a, b)
 #define ALPHA_BLEND_64_W(a, b)  interpolate<1,2>(a, b)
@@ -57,15 +55,15 @@ static uint32_t interpolate(uint32_t a, uint32_t b) {
 #define ALPHA_BLEND_192_W(a, b) interpolate<3,2>(a, b)
 #define ALPHA_BLEND_224_W(a, b) interpolate<7,3>(a, b)
 
-#define df(A, B) yuv_diff(A, B, src)
+#define df(A, B) yuv_diff(A, B)
 #define eq(A, B) (df(A, B) < 155)
 
 #define filt2b(PE, PI, PH, PF, PG, PC, PD, PB, PA, G5, C4, G0, D0, C1, B1, F4, I4, H5, I5, A0, A1, N0, N1, N2, N3) do { \
-    if (PE != PH && PE != PF) {                                                                     \
+    if (COLOR(PE) != COLOR(PH) && COLOR(PE) != COLOR(PF)) {                                                                     \
         const unsigned e = df(PE,PC) + df(PE,PG) + df(PI,H5) + df(PI,F4) + (df(PH,PF)<<2);          \
         const unsigned i = df(PH,PD) + df(PH,I5) + df(PF,I4) + df(PF,PB) + (df(PE,PI)<<2);          \
         if (e <= i) {                                                                               \
-            const unsigned px = df(PE,PF) <= df(PE,PH) ? PF : PH;                                   \
+            const unsigned px = df(PE,PF) <= df(PE,PH) ? COLOR(PF) : COLOR(PH);                                   \
             if (e < i && ( (!eq(PF,PB) && !eq(PH,PD)) || (eq(PE,PI) && (!eq(PF,I4) && !eq(PH,I5))) || eq(PE,PG) || eq(PE,PC)) ) { \
                 const unsigned ke = df(PF,PG);                                                      \
                 const unsigned ki = df(PH,PC);                                                      \
@@ -131,11 +129,11 @@ static uint32_t interpolate(uint32_t a, uint32_t b) {
 } while (0)
 
 #define filt4b(PE, PI, PH, PF, PG, PC, PD, PB, PA, G5, C4, G0, D0, C1, B1, F4, I4, H5, I5, A0, A1, N15, N14, N11, N3, N7, N10, N13, N12, N9, N6, N2, N1, N5, N8, N4, N0) do { \
-    if (PE != PH && PE != PF) {                                                                     \
+    if (COLOR(PE) != COLOR(PH) && COLOR(PE) != COLOR(PF)) {                                                                     \
         const unsigned e = df(PE,PC) + df(PE,PG) + df(PI,H5) + df(PI,F4) + (df(PH,PF)<<2);          \
         const unsigned i = df(PH,PD) + df(PH,I5) + df(PF,I4) + df(PF,PB) + (df(PE,PI)<<2);          \
         if (e <= i) {                                                                               \
-            const unsigned px = df(PE,PF) <= df(PE,PH) ? PF : PH;                                   \
+            const unsigned px = df(PE,PF) <= df(PE,PH) ? COLOR(PF) : COLOR(PH);                                   \
             if (e < i && ( (!eq(PF,PB) && !eq(PH,PD)) || (eq(PE,PI) && (!eq(PF,I4) && !eq(PH,I5))) || eq(PE,PG) || eq(PE,PC)) ) {\
                 const unsigned ke = df(PF,PG);                                                      \
                 const unsigned ki = df(PH,PC);                                                      \
@@ -209,35 +207,35 @@ static void scaleImpl(uint32_t *dst, int dstPitch, const uint8_t *src, int srcPi
 			// G0 PG PH PI I4
 			//    G5 H5 I5
 
-            const uint32_t B1 = &sa0[2] - src;
-            const uint32_t PB = &sa1[2] - src;
-            const uint32_t PE = &sa2[2] - src;
-            const uint32_t PH = &sa3[2] - src;
-            const uint32_t H5 = &sa4[2] - src;
+            const uint32_t B1 = sa0[2];
+            const uint32_t PB = sa1[2];
+            const uint32_t PE = sa2[2];
+            const uint32_t PH = sa3[2];
+            const uint32_t H5 = sa4[2];
 
             const int pprev = 2 - (x > 0);
-            const uint32_t A1 = &sa0[pprev] - src;
-            const uint32_t PA = &sa1[pprev] - src;
-            const uint32_t PD = &sa2[pprev] - src;
-            const uint32_t PG = &sa3[pprev] - src;
-            const uint32_t G5 = &sa4[pprev] - src;
+            const uint32_t A1 = sa0[pprev];
+            const uint32_t PA = sa1[pprev];
+            const uint32_t PD = sa2[pprev];
+            const uint32_t PG = sa3[pprev];
+            const uint32_t G5 = sa4[pprev];
 
             const int pprev2 = pprev - (x > 1);
-            const uint32_t A0 = &sa1[pprev2] - src;
-            const uint32_t D0 = &sa2[pprev2] - src;
-            const uint32_t G0 = &sa3[pprev2] - src;
+            const uint32_t A0 = sa1[pprev2];
+            const uint32_t D0 = sa2[pprev2];
+            const uint32_t G0 = sa3[pprev2];
 
             const int pnext = 3 - (x == w - 1);
-            const uint32_t C1 = &sa0[pnext] - src;
-            const uint32_t PC = &sa1[pnext] - src;
-            const uint32_t PF = &sa2[pnext] - src;
-            const uint32_t PI = &sa3[pnext] - src;
-            const uint32_t I5 = &sa4[pnext] - src;
+            const uint32_t C1 = sa0[pnext];
+            const uint32_t PC = sa1[pnext];
+            const uint32_t PF = sa2[pnext];
+            const uint32_t PI = sa3[pnext];
+            const uint32_t I5 = sa4[pnext];
 
             const int pnext2 = pnext + 1 - (x >= w - 2);
-            const uint32_t C4 = &sa1[pnext2] - src;
-            const uint32_t F4 = &sa2[pnext2] - src;
-            const uint32_t I4 = &sa3[pnext2] - src;
+            const uint32_t C4 = sa1[pnext2];
+            const uint32_t F4 = sa2[pnext2];
+            const uint32_t I4 = sa3[pnext2];
 
 			for (int j = 0; j < N; ++j) {
 				for (int i = 0; i < N; ++i) {
@@ -306,9 +304,9 @@ static void palette_xbr(const uint32_t *palette) {
 		const int r = (palette[i] >> 16) & 255;
 		const int g = (palette[i] >>  8) & 255;
 		const int b =  palette[i]        & 255;
-		_yuv[i * 3]     = ( 299 * r + 587 * g + 114 * b) * 4 / 1000;
-		_yuv[i * 3 + 1] = (-169 * r - 331 * g + 500 * b) * 4 / 1000 + 128;
-		_yuv[i * 3 + 2] = ( 500 * r - 419 * g -  81 * b) * 4 / 1000 + 128;
+		_yuv[i * 3]     = ( 299 * r + 587 * g + 114 * b) / 1000;
+		_yuv[i * 3 + 1] = (-169 * r - 331 * g + 500 * b) / 1000 + 128;
+		_yuv[i * 3 + 2] = ( 500 * r - 419 * g -  81 * b) / 1000 + 128;
 	}
 }
 
